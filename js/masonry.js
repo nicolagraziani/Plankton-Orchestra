a316781f1719856efa40654bb409aa75b6d7d2fc
Nicola Graziani <nicola.graziani92@gmail.com>
2022-04-13 17:47:30 +0200

    Aggiornamento sezione video

--- js/masonry.js
+++ js/masonry.js
@@ -2,12 +2,9 @@
 
 var elem = document.querySelector('.grid');
 var msnry = new Masonry( elem, {
-  // options
   itemSelector: '.grid-item',
 });
 
-// element argument can be a selector string
-//   for an individual element
 var msnry = new Masonry( '.grid', {
     gutter: 10
 });
